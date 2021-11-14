0c76f972927a49aa1dd97048ee30099232d2aa26
Alex Chasi <alexchasi17@gmail.com>
2021-11-14 02:07:19 -0500

    codigo de insertar datos en el formulario

--- S6AlexChasi/viewInsertarA.xaml.cs
+++ S6AlexChasi/viewInsertarA.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,6 +20,24 @@ namespace S6AlexChasi
 
         private void btnGuardar_Clicked(object sender, EventArgs e)
         {
+            try
+            {
+                WebClient cliente = new WebClient();
+                var parametros = new System.Collections.Specialized.NameValueCollection();
+
+                parametros.Add("codigo", txtCodigo.Text);
+                parametros.Add("nombre", txtNombre.Text);
+                parametros.Add("apellido", txtApellido.Text);
+                parametros.Add("edad", txtEdad.Text);
+
+                cliente.UploadValues("http://192.168.50.106:8084/moviles/post.php", "POST", parametros);
+
+                DisplayAlert("Mensaje", "Ingreso Correcto", "OK");
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Mensaje", ex.Message,"OK");
+            }
 
         }
 
