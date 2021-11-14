5760ada954a74f162c58dfbcfdbfcbc3e3912c7e
Alex Chasi <alexchasi17@gmail.com>
2021-11-14 01:08:07 -0500

    creado clase Datos

--- S6AlexChasi/Ws/Datos.cs
+++ S6AlexChasi/Ws/Datos.cs
@@ -6,5 +6,12 @@ namespace S6AlexChasi.Ws
 {
     class Datos
     {
+        public int codigo { get; set; }
+        public string nombre { get; set; }
+
+        public string apellido { get; set; }
+
+        public string edad { get; set; }
+
     }
 }
